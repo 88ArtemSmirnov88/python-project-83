b3f4b04a40ec547634d4e1072a43014ad1f1b712
88ArtemSmirnov88 <artembanchik@mail.ru>
2024-03-07 21:53:52 +0300

    init commit

---
+++ page_analyzer/app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
